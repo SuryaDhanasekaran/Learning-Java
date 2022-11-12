11d335e4c72087e72affbdfe086a1e5d23c57e39
SuryaThana <suryasedulous@gmail.com>
2022-11-12 19:33:27 +0530

    First Commit

---
+++ Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    private int doors;
+    private int wheels;
+    private String engine;
+    private String color;
+    private String model;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("porsche") || validModel.equals("holden")) {
+            this.model = model;
+        } else {
+
+            this.model = "Unknown";
+
+        }
+
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ SimpleCalculator.java
@@ -0,0 +1,56 @@
+public class SimpleCalculator {
+
+    private double firstNumber, secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double secondFirstNumber() {
+        return secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return secondNumber - firstNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+
+        if (secondNumber != 0) {
+            return firstNumber / secondNumber;
+        } else {
+            return 0;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+}
